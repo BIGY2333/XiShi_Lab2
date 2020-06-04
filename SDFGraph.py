2984c477d2b43e9a127d42f8c726cf74f202ebb7
sunwei96 <523727249@qq.com>
2020-06-04 19:32:28 +0800

    update-sunwei

--- SDFGraph.py
+++ SDFGraph.py
@@ -3,7 +3,8 @@ import graphviz
 import math
 import queue
 
-def ParamCheck(*ty2):
+# This function develops a set of decorators.
+def arg_type(*ty2):
     def common(fun):
         def deal(*fun_x):
             ty = map(ToCheckFun, ty2)
@@ -15,16 +16,14 @@ def ParamCheck(*ty2):
                     r = t_check(x_list_it.__next__())
                     result.append(r)
                 print('param check result: ', result)
-
             return fun(*fun_x)
-
         return deal
-
     return common
 
 def ToCheckFun(t):
     return lambda x: isinstance(x, t)
 
+# define a Synchronous Data Flow class
 class SDFGraph():
     def __init__(self, name):
         self.name = name
@@ -33,7 +32,8 @@ class SDFGraph():
         self.token_queues = []
         self.nodes = []
 
-    @ParamCheck(object, str, object, object, object)
+    # This function is used to build a node and put the output token and input token into queue.
+    @arg_type(object, str, object, object, object)
     def add_node(self, name, function, inputs, outputs):
         node = Node(name, function)
         node.inputs = self.join_queue(name, inputs, True)
@@ -41,7 +41,8 @@ class SDFGraph():
         self.nodes.append(node)
         return node
 
-    @ParamCheck(object, object)
+    # This function is used to judge whether the node is in nodes.
+    @arg_type(object, object)
     def get_node(self, name):
         current_name = name[0] if (type(name) == tuple) else name
         for value in self.nodes:
@@ -49,7 +50,8 @@ class SDFGraph():
                 return value
         return None
 
-    @ParamCheck(object, str, object, bool)
+    # This function is used to achieve the feature which put the output token and input token into queue in function add_node()
+    @arg_type(object, str, object, bool)
     def join_queue(self, current_name, node_list, input_flag):
         result = []
         if type(node_list) != list and type(node_list) != tuple:
@@ -90,13 +92,14 @@ class SDFGraph():
                             result.append(node.inputs[k])
         return result
 
-    @ParamCheck(object, int)
+    # This function is used to start to execute the data flow and set the clock.
+    @arg_type(object, int)
     def execute(self, clk):
         for i in range(clk):
             for node in self.nodes:
                 node.calculate()
 
-    @ParamCheck(object, object, int)
+    # This function is used to search the result of q's connection node.
     def back_queue(self, q, input_flag=True):
         res = []
         for i, n in enumerate(self.nodes):
@@ -108,6 +111,7 @@ class SDFGraph():
                 res.append(i)
         return res
 
+    # This function is used to generate the connection method of each node and output them in fsm.dot
     def visualize(self):
         res = []
         res.append("digraph G {")
@@ -134,7 +138,9 @@ class SDFGraph():
         res.append("}")
         return "\n".join(res)
 
-    @ParamCheck(object, object)
+    # This function is used to calculate the solution through direct calculation.
+    # The res is used to compare with the result calculated in Synchronous Data Flow in test function.
+    @arg_type(list)
     def quadratic(self, x):
         a = x[0]
         b = x[1]
@@ -147,6 +153,7 @@ class SDFGraph():
         else:
             print('No root')
 
+# define a Node class contain its input and output token and its function.
 class Node(object):
     def __init__(self, name, function):
         self.name = name
@@ -158,6 +165,7 @@ class Node(object):
         if current_queue.empty():
             return False
 
+    # This function is used to judge whether queue is empty.
     def if_activate(self):
         for current_queue in self.inputs:
             # if self.IsEmpty(current_queue):
@@ -165,6 +173,7 @@ class Node(object):
                 return False
         return True
 
+    # This function is used to get the input from queue.
     def get_input_from_queue(self):
         if not self.inputs:
             quit()
@@ -175,6 +184,7 @@ class Node(object):
                 get_input.append(input_queue.get())
         return get_input
 
+    # This function is used to put the output into queue.
     def put_output_to_queue(self, out):
         if not self.outputs:
             quit()
@@ -185,13 +195,15 @@ class Node(object):
         for i, output_queue in enumerate(self.outputs):
             output_queue.put(out[i])
 
+    # This function is used to calculate the result of the function of input.
     def calculate(self):
         get_input = self.get_input_from_queue()
-        if get_input is True:
+        if get_input:
             output = self.function(*get_input)
             self.put_output_to_queue(output)
             return output
 
+# This module is used to initialize nodes and graphs, then execute it and achieve visualization by GraphViz DOT.
 if __name__ == "__main__":
     SDF = SDFGraph('test')
     ina = queue.Queue(2)
@@ -203,15 +215,15 @@ if __name__ == "__main__":
     output1 = queue.Queue(2)
     output2 = queue.Queue(2)
 
-    SDF.add_node('a', lambda x: [x, x], ina, ['2a', ('4ac', 0)])
-    SDF.add_node('b', lambda x: [x, x], inb, ['b^2-4ac', ('molecular', 0)])
-    SDF.add_node('c', lambda x: x, inc, [('b^2-4ac', 1)])
-    SDF.add_node('2a', lambda x: [2 * x, 2 * x], [('a', 0)], [('root1', 1), ('root2', 1)])
-    SDF.add_node('b^2-4ac', lambda x, a, c: x ** 2 - 4 * a * c, [('a', 1), 'c'], [('sqrt', 1)])
-    SDF.add_node('sqrt', lambda x: x ** 0.5, ['b^2-4ac'], [('molecular', 1)])
-    SDF.add_node('molecular', lambda x, y: [-x - y, -x + y], [('b', 1), 'sqrt'], [('root1', 0), ('root2', 0)])
-    SDF.add_node('root1', lambda x, y: x / y, [('molecular', 0), ('2a', 0)], output1)
-    SDF.add_node('root2', lambda x, y: x / y, [('molecular', 1), ('2a', 1)], output2)
+    SDF.add_node('a', lambda x: [x, x], ina, ['2a', '4ac'])
+    SDF.add_node('b', lambda x: [x, x], inb, ['b^2-4ac', 'molecular'])
+    SDF.add_node('d', lambda x: x, inc, ['b^2-4ac'])
+    SDF.add_node('2a', lambda x: [2 * x, 2 * x], ['a'], ['root1', 'root2'])
+    SDF.add_node('b^2-4ac', lambda x, a, c: x ** 2 - 4 * a * c, ['a', 'c'], ['sqrt'])
+    SDF.add_node('sqrt', lambda x: x ** 0.5, ['b^2-4ac'], ['molecular'])
+    SDF.add_node('molecular', lambda x, y: [-x - y, -x + y], ['b', 'sqrt'], ['root1', 'root2'])
+    SDF.add_node('root1', lambda x, y: x / y, ['molecular', '2a'], output1)
+    SDF.add_node('root2', lambda x, y: x / y, ['molecular', '2a'], output2)
 
     SDF.execute(10)
 
@@ -219,7 +231,7 @@ if __name__ == "__main__":
     f = open('fsm.dot', 'w')
     f.write(g)
     f.close()
-    # with open("fsm.dot") as f:
-    #     dot_graph = f.read()
-    # dot = graphviz.Source(dot_graph)
-    # dot.view()
+    with open("fsm.dot") as f:
+        dot_graph = f.read()
+    dot = graphviz.Source(dot_graph)
+    dot.view()
--- SDFGraph_test.py
+++ SDFGraph_test.py
@@ -2,42 +2,84 @@ import unittest
 import math
 import random
 from SDFGraph import *
+from datetime import datetime
+import logging
 
 class SDFGraph_test(unittest.TestCase):
     def test_SDFGraph(self):
         SDF = SDFGraph('test')
+        begin = datetime.now().microsecond
+        # ina inb and inc are represent a b and c, their type are node.
         ina = queue.Queue(2)
         inb = queue.Queue(2)
         inc = queue.Queue(2)
+        # output1 and output2 are represent two roots, their type are node.
         output1 = queue.Queue(3)
         output2 = queue.Queue(3)
+        end = datetime.now().microsecond
+        print(end - begin)
+        logging.info("The queues are set")
 
-        SDF.add_node('a', lambda x: [x, x], ina, ['2a', ('4ac', 0)])
-        SDF.add_node('b', lambda x: [x, x], inb, ['b^2-4ac', ('molecular', 0)])
-        SDF.add_node('c', lambda x: x, inc, [('b^2-4ac', 1)])
-        SDF.add_node('2a', lambda x: [2 * x, 2 * x], [('a', 0)], [('root1', 1), ('root2', 1)])
-        SDF.add_node('b^2-4ac', lambda x, a, c: x**2 - 4 * a * c, [('a', 1), 'c'], [('sqrt', 1)])
-        SDF.add_node('sqrt', lambda x: x**0.5, ['b^2-4ac'], [('molecular', 1)])
-        SDF.add_node('molecular', lambda x, y: [-x - y, -x + y], [('b', 1), 'sqrt'], [('root1', 0), ('root2', 0)])
-        SDF.add_node('root1', lambda x, y: x / y, [('molecular', 0), ('2a', 0)], output1)
-        SDF.add_node('root2', lambda x, y: x / y, [('molecular', 1), ('2a', 1)], output2)
+        begin=datetime.now().microsecond
+        SDF.add_node('a', lambda x: [x, x], ina, ['2a', '4ac'])
+        SDF.add_node('b', lambda x: [x, x], inb, ['b^2-4ac', 'molecular'])
+        SDF.add_node('d', lambda x: x, inc, ['b^2-4ac'])
+        SDF.add_node('2a', lambda x: [2 * x, 2 * x], ['a'], ['root1', 'root2'])
+        SDF.add_node('b^2-4ac', lambda x, a, c: x ** 2 - 4 * a * c, ['a', 'c'], ['sqrt'])
+        SDF.add_node('sqrt', lambda x: x ** 0.5, ['b^2-4ac'], ['molecular'])
+        SDF.add_node('molecular', lambda x, y: [-x - y, -x + y], ['b', 'sqrt'], ['root1', 'root2'])
+        SDF.add_node('root1', lambda x, y: x / y, ['molecular', '2a'], output1)
+        SDF.add_node('root2', lambda x, y: x / y, ['molecular', '2a'], output2)
+        end = datetime.now().microsecond
+        print(end - begin)
+        logging.info("The nodes are added into queue")
 
+        begin = datetime.now().microsecond
         abc = [(1, 2, 1)]
         for x in abc:
             expect = SDF.quadratic(x)
             ina.put(x[0])
             inb.put(x[1])
             inc.put(x[2])
-            SDF.execute(10)
+            SDF.execute(15)
             self.assertEqual([-1.0, -1.0], expect)
+        end = datetime.now().microsecond
+        print(end - begin)
+        logging.info("The process is ending")
+
+    #Dear professor,here is a SR model but I dont know how to implement it concretely  0..0
+    def test_SDFGraph_SR(self):
+        Q = 1
+        S = 0
+        R = 1
+        if S:
+            Q = 0
+        elif R:
+            Q = 1
+        else:
+            Q = Q
+        if Q == 1:
+            print("The process is starting") #do the process
+
+    def test_SDF_with_One_Node(self):
+        SDF = SDFGraph('test1')
+        ina = queue.Queue(2)
+        output = queue.Queue(3)
+        SDF.add_node('d', lambda x: x**2, ina, output)
+        d = [2]
+        expect = [d[0] * d[0]]
+        ina.put(d[0])
+        SDF.execute(2)
+        res = [output.get()]
+        self.assertEqual(res, expect)
 
 class node_test(unittest.TestCase):
     def test_node_sin(self):
         f = lambda x: math.sin(x)
-        node = Node('add', f)
-        x = queue.Queue(10)
-        y = queue.Queue(10)
-        xs = [random.random() for i in range(10)]
+        node = Node('sin', f)
+        x = queue.Queue(3)
+        y = queue.Queue(3)
+        xs = [1, 2, 3]
         for k in xs:
             x.put(k)
             node.inputs = [x]
@@ -48,12 +90,15 @@ class node_test(unittest.TestCase):
     def test_node_add(self):
         f = lambda x: x + x
         node = Node('add', f)
-        x = queue.Queue(10)
-        y = queue.Queue(10)
-        xs = [random.random() for i in range(10)]
+        x = queue.Queue(3)
+        y = queue.Queue(3)
+        xs = [1, 2, 3]
         for k in xs:
             x.put(k)
             node.inputs = [x]
             node.outputs = [y]
             node.calculate()
             self.assertEqual(y.get(), f(k))
+
+if __name__ == '__main__':
+    unittest.main()
